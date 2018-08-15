1f23cbfb67f2b227b187910f58a67fbedddd85d4
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2018-08-15 15:30:54 +0100

    two numbers use case

--- src/main/java/com/blacknebula/kata/StringCalculator.java
+++ src/main/java/com/blacknebula/kata/StringCalculator.java
@@ -5,12 +5,22 @@ public class StringCalculator {
     /**
      * @should return zero if input is an empty string
      * @should return same value if one number is passed
+     * @should return sum if two numbers are passed
      */
     public int add(String input) {
         if (input.length() == 0) {
             return 0;
         }
+        final String[] numbers = input.split(",");
 
-        return Integer.parseInt(input);
+        if (numbers.length == 1) {
+            return parseInt(numbers[0]);
+        }
+        return parseInt(numbers[0]) + parseInt(numbers[1]);
     }
+
+    private int parseInt(String number) {
+        return Integer.parseInt(number);
+    }
+
 }
--- src/test/java/com/blacknebula/kata/StringCalculatorTest.java
+++ src/test/java/com/blacknebula/kata/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.blacknebula.kata;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -45,4 +46,19 @@ public class StringCalculatorTest {
         Assertions.assertThat(result).isEqualTo(4);
     }
 
+    /**
+     * @verifies return sum if two numbers are passed
+     * @see StringCalculator#add(String)
+     */
+    @Test
+    public void add_shouldReturnSumIfTwoNumbersArePassed() throws Exception {
+        // given
+        final String input = "1,3";
+
+        // when
+        final int result = stringCalculator.add(input);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(4);
+    }
 }
