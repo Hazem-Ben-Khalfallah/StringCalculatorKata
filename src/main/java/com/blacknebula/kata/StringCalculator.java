676e7a53ed1ed271862f9781e260001f29ee0114
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2018-08-15 15:02:21 +0100

    empty string use case

--- src/main/java/com/blacknebula/kata/StringCalculator.java
+++ src/main/java/com/blacknebula/kata/StringCalculator.java
@@ -1,4 +1,11 @@
 package com.blacknebula.kata;
 
 public class StringCalculator {
+
+    /**
+     * @should return zero if input is an empty string
+     */
+    public int add(String input) {
+        return 0;
+    }
 }
--- src/test/java/com/blacknebula/kata/StringCalculatorTest.java
+++ src/test/java/com/blacknebula/kata/StringCalculatorTest.java
@@ -1,5 +1,25 @@
 package com.blacknebula.kata;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
+
+    /**
+     * @verifies return zero if input is an empty string
+     * @see StringCalculator#add(String)
+     */
+    @Test
+    public void add_shouldReturnZeroIfInputIsAnEmptyString() {
+        // given
+        final String input = "";
+        final StringCalculator stringCalculator = new StringCalculator();
+
+        // when
+        final int result = stringCalculator.add(input);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(0);
+    }
 }
