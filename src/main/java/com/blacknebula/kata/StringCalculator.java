221bc054e70450f76eef34af8f814f029f5f2cef
Hazem Ben Khalfallah <hazem.benkhalfallah@influans.com>
2018-08-15 15:14:28 +0100

    one number use case

--- src/main/java/com/blacknebula/kata/StringCalculator.java
+++ src/main/java/com/blacknebula/kata/StringCalculator.java
@@ -4,8 +4,13 @@ public class StringCalculator {
 
     /**
      * @should return zero if input is an empty string
+     * @should return same value if one number is passed
      */
     public int add(String input) {
-        return 0;
+        if (input.length() == 0) {
+            return 0;
+        }
+
+        return Integer.parseInt(input);
     }
 }
--- src/test/java/com/blacknebula/kata/StringCalculatorTest.java
+++ src/test/java/com/blacknebula/kata/StringCalculatorTest.java
@@ -1,10 +1,17 @@
 package com.blacknebula.kata;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() {
+        stringCalculator = new StringCalculator();
+    }
 
     /**
      * @verifies return zero if input is an empty string
@@ -14,7 +21,6 @@ public class StringCalculatorTest {
     public void add_shouldReturnZeroIfInputIsAnEmptyString() {
         // given
         final String input = "";
-        final StringCalculator stringCalculator = new StringCalculator();
 
         // when
         final int result = stringCalculator.add(input);
@@ -22,4 +28,21 @@ public class StringCalculatorTest {
         // then
         Assertions.assertThat(result).isEqualTo(0);
     }
+
+    /**
+     * @verifies return same value if one number is passed
+     * @see StringCalculator#add(String)
+     */
+    @Test
+    public void add_shouldReturnSameValueIfOneNumberIsPassed() {
+        // given
+        final String input = "4";
+
+        // when
+        final int result = stringCalculator.add(input);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(4);
+    }
+
 }
